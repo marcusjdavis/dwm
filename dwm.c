769652da2421d3807ac6820964133b46c93bd5f8
Marcus Davis <marcus.james.davis@outlook.com>
2019-06-11 17:54:42 +0100

    Patching dwm with autostart

--- dwm.c
+++ dwm.c
@@ -198,6 +198,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void runAuto(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1441,6 +1442,11 @@ run(void)
 }
 
 void
+runAuto(void) {
+  system("cd ~/.dwm; ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2243,6 +2249,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAuto();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
